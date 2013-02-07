d36a94eaa8b08b119bea21ea9717b316e6298891
Kyle Isom <coder@kyleisom.net>
2013-02-06 22:52:16 -0700

    initial commit

---
+++ autobuild.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# performs a full clean, autoreconf, configure with appropriate options,
+# make, and make check: it starts from scratch, with a clean dev checkout
+# and performs everything necessary to run the unit tests.
+
+rm core*
+echo "[+] performing autobuild" ;          \
+        git clean -Xdf                  && \
+        autoreconf -i                   && \
+        ./configure CC=clang            && \
+        make clean                      && \
+        make                            && \
+        make check
